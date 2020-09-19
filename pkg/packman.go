bd1387f053db05b65f87d5eb2ee78f7ba2253d5f
Domrev Igor <domrevigor@gmail.com>
2020-09-19 21:48:59 +0300

    allowing authenticatino as lib

--- pkg/packman.go
+++ pkg/packman.go
@@ -7,3 +7,7 @@ import (
 func Unpack(remote, path string, flagsMap map[string]string) error {
 	return internal.M.TemplatingService.Unpack(remote, path, flagsMap)
 }
+
+func Auth(username, password string) error {
+	return internal.M.ConfigService.SetAuth(username, password)
+}
